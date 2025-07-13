7745fee0bf567ff00e68c2e43791fef18ea001e9
Arthur <arthur@MacBook-Pro-de-Arthur.local>
2025-07-13 17:26:48 +0200

    add du mail

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend';
 
 import { EmailTemplate } from '@/components/auth/email-template';
 
-export const resend = new Resend(process.env.RESEND_API_KEY);
+export const resend = new Resend("re_FesZ6D6o_QGm8JARrBESUmcJqwCtKGb22");
 
 // export const sendVerificationEmail = async (email: string, token: string) => {
 //     const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
